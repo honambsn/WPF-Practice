ffb6c98c5d37da999629c00a179f52cc792726c6
honam <75104291+honambsn@users.noreply.github.com>
2024-08-30 10:51:06 +0700

    update: new proj, firstlook

---
+++ Modern UI/Design TV/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Design_TV
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private DispatcherTimer _timer;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			StartClock();
+			StartDateUpdate();
+		}
+
+		private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+
+		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+
+		private void StartClock()
+		{
+			_timer = new DispatcherTimer();
+			_timer.Interval = TimeSpan.FromSeconds(1); // Update every second
+			_timer.Tick += Timer_Tick;
+			_timer.Start();
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			// Update the TextBlock with the current time
+			timeTextBlock.Text = DateTime.Now.ToString("HH:mm");
+		}
+
+		private void StartDateUpdate()
+		{
+			_timer = new DispatcherTimer();
+			_timer.Interval = TimeSpan.FromMinutes(1); // Update every minute
+			_timer.Tick += Timer_Tick;
+			_timer.Start();
+
+			// Initial update
+			Date_Tick(null, null);
+		}
+
+		private void Date_Tick(object sender, EventArgs e)
+		{
+			// Update the TextBlocks with the current date and day of the week
+			var now = DateTime.Now;
+			dateTextBlock.Text = now.ToString("MMMM, dddd"); // Format for "Nov, 17"
+			dayTextBlock.Text = now.ToString("dddd"); // Format for "Tuesday"
+		}
+
+		private void backButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
---
+++ Modern UI/Design TV/UserControls/TimeSetting.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Design_TV.UserControls
+{
+	/// <summary>
+	/// Interaction logic for TimeSetting.xaml
+	/// </summary>
+	public partial class TimeSetting : UserControl
+	{
+		public TimeSetting()
+		{
+			InitializeComponent();
+		}
+		public string Title
+		{
+			get { return (string)GetValue(TitleProperty); }
+			set { SetValue(TitleProperty, value); }
+		}
+		public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(TimeSetting));
+
+
+		public bool HasToggle
+		{
+			get { return (bool)GetValue(HasToggleProperty); }
+			set { SetValue(HasToggleProperty, value); }
+		}
+		public static readonly DependencyProperty HasToggleProperty = DependencyProperty.Register("HasToggle", typeof(bool), typeof(TimeSetting));
+
+
+		public bool IsChecked
+		{
+			get { return (bool)GetValue(IsCheckedProperty); }
+			set { SetValue(IsCheckedProperty, value); }
+		}
+		public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register("IsChecked", typeof(bool), typeof(TimeSetting));
+
+
+		public bool HasText
+		{
+			get { return (bool)GetValue(HasTextProperty); }
+			set { SetValue(HasTextProperty, value); }
+		}
+		public static readonly DependencyProperty HasTextProperty = DependencyProperty.Register("HasText", typeof(bool), typeof(TimeSetting));
+
+
+		public string Text
+		{
+			get { return (string)GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
+		}
+		public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(TimeSetting));
+
+	}
+}
