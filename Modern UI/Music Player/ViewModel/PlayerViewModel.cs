d892c2185c9376784c7e6cd186fe2fa50a0ae68b
honam <75104291+honambsn@users.noreply.github.com>
2024-09-04 10:48:08 +0700

    update: update size

---
+++ Modern UI/Music Player/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,97 @@
+﻿//using MahApps.Metro.IconPacks;
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Input;
+
+//namespace Music_Player.ViewModel
+//{
+//	public class PlayerViewModel : INotifyPropertyChanged
+//	{
+//		private PackIconMaterialKind _iconKind;
+
+//		public PlayerViewModel()
+//		{
+//			// Initialize the icon to Play
+//			IconKind = PackIconMaterialKind.Play;
+
+//			// Initialize the command
+//			ToggleCommand = new RelayCommand(ToggleIcon);
+//		}
+
+//		public PackIconMaterialKind IconKind
+//		{
+//			get => _iconKind;
+//			set
+//			{
+//				if (_iconKind != value)
+//				{
+//					_iconKind = value;
+//					OnPropertyChanged();
+//				}
+//			}
+//		}
+
+//		public ICommand ToggleCommand { get; }
+
+//		private void ToggleIcon()
+//		{
+//			IconKind = IconKind == PackIconMaterialKind.Play
+//				? PackIconMaterialKind.Pause
+//				: PackIconMaterialKind.Play;
+//		}
+
+//		public event PropertyChangedEventHandler PropertyChanged;
+
+//		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+//		{
+//			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//		}
+//	}
+//	public class PlayerViewModel : INotifyPropertyChanged
+//	{
+//		private PackIconMaterialKind _iconKind;
+
+//		public PlayerViewModel()
+//		{
+//			// Initialize the icon to Play
+//			IconKind = PackIconMaterialKind.Play;
+
+//			// Initialize the command
+//			ToggleCommand = new RelayCommand(ToggleIcon);
+//		}
+
+//		public PackIconMaterialKind IconKind
+//		{
+//			get => _iconKind;
+//			set
+//			{
+//				if (_iconKind != value)
+//				{
+//					_iconKind = value;
+//					OnPropertyChanged();
+//				}
+//			}
+//		}
+
+//		public ICommand ToggleCommand { get; }
+
+//		private void ToggleIcon()
+//		{
+//			IconKind = IconKind == PackIconMaterialKind.Play
+//				? PackIconMaterialKind.Pause
+//				: PackIconMaterialKind.Play;
+//		}
+
+//		public event PropertyChangedEventHandler PropertyChanged;
+
+//		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+//		{
+//			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//		}
+//	}
+//}
