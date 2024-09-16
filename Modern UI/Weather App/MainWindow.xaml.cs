702e1205ea221291d9a666140612abb32f115896
honam <75104291+honambsn@users.noreply.github.com>
2024-09-16 10:30:11 +0700

    update: update add api

--- Modern UI/Weather App/MainWindow.xaml.cs
+++ Modern UI/Weather App/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Weather_App.ViewModel;
 
 namespace Weather_App
 {
@@ -23,6 +27,11 @@ namespace Weather_App
 		public MainWindow()
 		{
 			InitializeComponent();
+			_viewModel = new WeatherViewModel();
+			DataContext = _viewModel;
+
+			// Optionally, you can fetch the weather data when the window loads
+			LoadWeatherData();
 		}
 
 		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
@@ -46,6 +55,44 @@ namespace Weather_App
 			{
 				textSearch.Visibility = Visibility.Visible;
 			}	
+		}
+
+
+		private WeatherViewModel _viewModel;
+
+		private const string ApiKey = "d4c56fcae54bbc960c8385227bee335b";
+		private const string CityId = "1566083";
+		private const string BaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+
+		private async void LoadWeatherData()
+		{
+			string url = $"{BaseUrl}?id={CityId}&appid={ApiKey}&units=metric";
+
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					HttpResponseMessage response = await client.GetAsync(url);
+					response.EnsureSuccessStatusCode();
+					string responseBody = await response.Content.ReadAsStringAsync();
+
+					// Parse the JSON data
+					JObject weatherData = JObject.Parse(responseBody);
+					string temperature = weatherData["main"]["temp"].ToString();
+					string description = weatherData["weather"][0]["description"].ToString();
+					string icon = weatherData["weather"][0]["icon"].ToString(); // Get the weather icon code
+
+					// Update ViewModel properties
+					_viewModel.Description = $"{description}";
+					_viewModel.Temperature = $"{temperature}°C";
+					_viewModel.IconUrl = $"http://openweathermap.org/img/wn/{icon}@2x.png";
+				}
+				catch (HttpRequestException ex)
+				{
+					_viewModel.Description = $"Error: {ex.Message}";
+				}
+			}
 		}
 	}
 }
---
+++ Modern UI/Weather App/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_App.ViewModel
+{
+	public class WeatherViewModel : INotifyPropertyChanged
+	{
+		private string _description;
+		private string _temperature;
+		private string _iconUrl;
+
+		public string Description
+		{
+			get { return _description; }
+			set
+			{
+				_description = value;
+				OnPropertyChanged("Description");
+			}
+		}
+
+		public string Temperature
+		{
+			get { return _temperature; }
+			set
+			{
+				_temperature = value;
+				OnPropertyChanged("Temperature");
+			}
+		}
+
+		public string IconUrl
+		{
+			get { return _iconUrl; }
+			set
+			{
+				_iconUrl = value;
+				OnPropertyChanged("IconUrl");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string name)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+	}
+}
