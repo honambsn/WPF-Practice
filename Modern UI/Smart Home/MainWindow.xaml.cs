8c2d0a4256045f790e8e452bf17761256d592183
honam <75104291+honambsn@users.noreply.github.com>
2024-09-23 08:32:13 +0700

    update: complete 2 tabs

--- Modern UI/Smart Home/MainWindow.xaml.cs
+++ Modern UI/Smart Home/MainWindow.xaml.cs
@@ -27,12 +27,37 @@ namespace Smart_Home
 
 		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
 		{
+			if(e.ChangedButton == MouseButton.Left)
+			{
+				this.DragMove();
+			}
 
-        }
+		}
 
+		bool IsMaximized = false;
 		private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
 		{
+			if(e.ClickCount == 2)
+			{
+				if (IsMaximized)
+				{
+					this.WindowState = WindowState.Normal;
+					this.Width = 1080;
+					this.Height = 720;
 
-        }
+					IsMaximized = false;
+				}
+				else
+				{
+					this.WindowState = WindowState.Maximized;
+
+					IsMaximized = true;
+
+				}
+			}
+
+		}
+
+
     }
 }
---
+++ Modern UI/Smart Home/UserControls/AddButton.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Smart_Home.UserControls
+{
+	/// <summary>
+	/// Interaction logic for AddButton.xaml
+	/// </summary>
+	public partial class AddButton : UserControl
+	{
+		public AddButton()
+		{
+			InitializeComponent();
+		}
+
+		public string Title
+		{
+			get { return (string)GetValue(TitleProperty); }
+			set { SetValue(TitleProperty, value); }
+		}
+
+		public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(AddButton));
+	}
+}
