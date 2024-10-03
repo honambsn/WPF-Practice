f3bf7982893e69b96ca2765e982d301d0b52c79c
honam <75104291+honambsn@users.noreply.github.com>
2024-10-03 10:12:54 +0700

    upadate: update ui, func later

--- Modern UI/Fitness App/UserControls/InfoCard.xaml.cs
+++ Modern UI/Fitness App/UserControls/InfoCard.xaml.cs
@@ -25,12 +25,15 @@ namespace Fitness_App.UserControls
 		{
 			InitializeComponent();
 		}
+
 		public string Title
 		{
 			get { return (string)GetValue(TitleProperty); }
 			set { SetValue(TitleProperty, value); }
 		}
-		public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(TextInfo));
+
+		public static readonly DependencyProperty TitleProperty =
+			DependencyProperty.Register("Title", typeof(string), typeof(InfoCard));
 
 		public string Desc
 		{
@@ -38,23 +41,26 @@ namespace Fitness_App.UserControls
 			set { SetValue(DescProperty, value); }
 		}
 
-		public static readonly DependencyProperty DescProperty = DependencyProperty.Register("Desc", typeof(string), typeof(TextInfo));
+		public static readonly DependencyProperty DescProperty =
+			DependencyProperty.Register("Desc", typeof(string), typeof(InfoCard));
 
 		public string Percentage
 		{
 			get { return (string)GetValue(PercentageProperty); }
 			set { SetValue(PercentageProperty, value); }
-
 		}
-		public static readonly DependencyProperty PercentageProperty = DependencyProperty.Register("Percentage", typeof(string), typeof(TextInfo));
 
+		public static readonly DependencyProperty PercentageProperty =
+			DependencyProperty.Register("Percentage", typeof(string), typeof(InfoCard));
 
 		public string BottomText
 		{
 			get { return (string)GetValue(BottomTextProperty); }
 			set { SetValue(BottomTextProperty, value); }
 		}
-		public static readonly DependencyProperty BottomTextProperty = DependencyProperty.Register("BottomText", typeof(string), typeof(TextInfo));
+
+		public static readonly DependencyProperty BottomTextProperty =
+			DependencyProperty.Register("BottomText", typeof(string), typeof(InfoCard));
 
 		public string Days
 		{
@@ -62,27 +68,34 @@ namespace Fitness_App.UserControls
 			set { SetValue(DaysProperty, value); }
 		}
 
-		public static readonly DependencyProperty DaysProperty = DependencyProperty.Register("Days", typeof(string), typeof(TextInfo));
+		public static readonly DependencyProperty DaysProperty =
+			DependencyProperty.Register("Days", typeof(string), typeof(InfoCard));
 
 		public int Value
 		{
 			get { return (int)GetValue(ValueProperty); }
 			set { SetValue(ValueProperty, value); }
 		}
-		public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(int), typeof(TextInfo));
+
+		public static readonly DependencyProperty ValueProperty =
+			DependencyProperty.Register("Value", typeof(int), typeof(InfoCard));
 
 		public bool IsActive
 		{
 			get { return (bool)GetValue(IsActiveProperty); }
 			set { SetValue(IsActiveProperty, value); }
 		}
-		public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(InfoCard));
+
+		public static readonly DependencyProperty IsActiveProperty =
+			DependencyProperty.Register("IsActive", typeof(bool), typeof(InfoCard));
 
 		public MahApps.Metro.IconPacks.PackIconMaterialKind Icon
 		{
 			get { return (MahApps.Metro.IconPacks.PackIconMaterialKind)GetValue(IconProperty); }
 			set { SetValue(IconProperty, value); }
 		}
-		public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(MahApps.Metro.IconPacks.PackIconMaterialKind), typeof(TextInfo));
+
+		public static readonly DependencyProperty IconProperty =
+			DependencyProperty.Register("Icon", typeof(MahApps.Metro.IconPacks.PackIconMaterialKind), typeof(InfoCard));
 	}
 }
