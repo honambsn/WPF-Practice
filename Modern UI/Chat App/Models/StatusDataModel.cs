78b97480a8b1e5f2d2df53db9e616e1e9967450c
honam <75104291+honambsn@users.noreply.github.com>
2024-10-10 09:48:04 +0700

    update: contact status thumbnails, binding viewmodel to itemcontrol

---
+++ Modern UI/Chat App/Models/StatusDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_App.Models
+{
+	public class StatusDataModel
+	{
+		public string ContactName { get; set; }
+		public Uri ContactPhoto { get; set; }
+		public Uri StatusImage { get; set; }
+		public bool IsMeAddStatus { get; set; }
+	}
+}
---
+++ Modern UI/Chat App/ViewModels/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Chat_App.CustomControls;
+using Chat_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_App.ViewModels
+{
+	public class ViewModel
+	{
+		public ObservableCollection<StatusDataModel> statusThumbsCollection;
+		public ViewModel()
+		{
+			statusThumbsCollection = new ObservableCollection<StatusDataModel>()
+			{
+				new StatusDataModel
+				{
+					IsMeAddStatus=true
+				},
+				new StatusDataModel
+				{
+					ContactName="Rosé",
+					ContactPhoto=new Uri("/assets/img1.jpg", UriKind.RelativeOrAbsolute),
+					StatusImage=new Uri("/assets/img5.jpg", UriKind.RelativeOrAbsolute),
+					IsMeAddStatus=false
+				},
+				new StatusDataModel
+				{
+					ContactName="Rosé",
+					ContactPhoto=new Uri("/assets/img1.jpg", UriKind.RelativeOrAbsolute),
+					StatusImage=new Uri("/assets/img5.jpg", UriKind.RelativeOrAbsolute),
+					IsMeAddStatus=false
+				},
+			};
+		}
+	}
+}
