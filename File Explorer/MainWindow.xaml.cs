cfae5c74fe622b0c4e038f849e3e7f6b745752ec
honam <75104291+honambsn@users.noreply.github.com>
2024-12-10 10:36:39 +0700

    update: styling window, side navigation(err existed)

--- File Explorer/MainWindow.xaml.cs
+++ File Explorer/MainWindow.xaml.cs
@@ -24,5 +24,27 @@ namespace File_Explorer
 		{
 			InitializeComponent();
 		}
-	}
+
+		private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Minimized;
+		}
+
+		private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (WindowState == WindowState.Maximized)
+			{
+				WindowState = WindowState.Normal;
+			}
+			else
+			{
+				WindowState = WindowState.Maximized;
+			}
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+    }
 }
