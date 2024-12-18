774689dcd6057672bfa8c3b99dfa841a8a4bfd1a
honam <75104291+honambsn@users.noreply.github.com>
2024-12-18 10:21:05 +0700

    update: complete drive & folder btn, explorer header, menu header btn, expand/collapse menu header toggle btn

--- Files Explorer/CustomControls/DriveAndFolderButton.cs
+++ Files Explorer/CustomControls/DriveAndFolderButton.cs
@@ -11,18 +11,39 @@ namespace File_Explorer.CustomControls
 {
 	internal class DriveAndFolderButton : RadioButton
 	{
+		//public PathGeometry Icon
+		//{
+		//	get
+		//	{
+		//		return (PathGeometry)GetValue(IconProperty);
+		//	}
+		//	set
+		//	{
+		//		SetValue(IconProperty, value);
+		//	}
+		//}
+
+		//public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(DriveAndFolderButton));
+
+
 		public PathGeometry Icon
 		{
-			get
-			{
-				return (PathGeometry)GetValue(IconProperty);
-			}
-			set
-			{
-				SetValue(IconProperty, value);
-			}
+			get { return (PathGeometry)GetValue(IconProperty); }
+			set { SetValue(IconProperty, value); }
 		}
 
-		public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(DriveAndFolderButton));
+		public static readonly DependencyProperty IconProperty =
+			DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(DriveAndFolderButton));
+
+		// New Folder property
+		public static readonly DependencyProperty FolderProperty =
+			DependencyProperty.Register("Folder", typeof(Geometry), typeof(DriveAndFolderButton));
+
+		public Geometry Folder
+		{
+			get { return (Geometry)GetValue(FolderProperty); }
+			set { SetValue(FolderProperty, value); }
+		}
+
 	}
 }
--- Files Explorer/Views/MainWindow.xaml.cs
+++ Files Explorer/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Files_Explorer
+namespace Files_Explorer.Views
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
