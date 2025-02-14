c904602f54f2af0825fd29747222fdaa9afea55b
honam <75104291+honambsn@users.noreply.github.com>
2025-02-14 08:53:52 +0700

    update: pawn move

--- Chess/ChessLogic/Pieces/Pawn.cs
+++ Chess/ChessLogic/Pieces/Pawn.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,18 @@ namespace ChessLogic
 		public override Player Color { get; }
 		//public bool HasMoved { get; set; } = false;
 
+		private readonly Direction forward;
 		public Pawn(Player color)
 		{
 			Color = color;
+			if (color == Player.White)
+			{
+				forward = Direction.North;
+			}
+			else
+			{
+				forward = Direction.South;
+			}
 		}
 
 		public override Piece Copy()
@@ -22,6 +32,55 @@ namespace ChessLogic
 			Pawn copy = new Pawn(Color);
 			copy.HasMoved = HasMoved;
 			return copy;
+		}
+
+		private static bool CanMoveTo(Position pos, Board board)
+		{
+			return Board.IsInside(pos) && board.IsEmpty(pos);
+		}
+
+		private bool CanCaptureAt(Position pos, Board board)
+		{
+			if (!Board.IsInside(pos) || board.IsEmpty(pos))
+			{
+				return false;
+			}
+			return board[pos].Color != Color;
+		}
+
+		private IEnumerable<Move> ForwardMoves(Position from, Board board)
+		{
+			Position oneMovePos = from + forward;
+
+			if (CanMoveTo(oneMovePos, board))
+			{
+				yield return new NormalMove(from, oneMovePos);
+
+				Position twoMovePos = oneMovePos + forward;
+
+				if (!HasMoved && CanMoveTo(twoMovePos, board))
+				{
+					yield return new NormalMove(from, twoMovePos);
+				}
+			}
+		}
+
+		private IEnumerable<Move> DiagonalMoves(Position from, Board board)
+		{
+			foreach( Direction dir in new Direction[] { Direction.West, Direction.East})
+			{
+				Position to = from + forward + dir;
+
+				if (CanCaptureAt(to, board))
+				{
+					yield return new NormalMove(from, to);
+				}
+			}
+		}
+
+		public override IEnumerable<Move> GetMoves(Position from, Board board)
+		{
+			return ForwardMoves(from, board).Concat(DiagonalMoves(from, board));
 		}
 	}
 }
